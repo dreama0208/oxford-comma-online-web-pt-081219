99a0d0f7c60b84fe754d4ef90468b66e09956aa0
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-20 21:21:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+    array.join(" and ")
+  else 
+    a_l = array.pop
+    array.join(", ") << " and " << a_l
+  end
+    
+end 
